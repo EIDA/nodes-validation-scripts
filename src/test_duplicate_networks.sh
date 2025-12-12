78de5d28185fb0e3e8ea4ca8d1d65c7e12f7a62d
Jonathan Schaeffer <jonathan.schaeffer@univ-grenoble-alpes.fr>
2025-12-12 10:56:50 +0100

    Test for duplicate networks

---
+++ src/test_duplicate_networks.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+source lib/nodes-tester.bash
+
+for node in $NODE_DEFINITIONS; do
+    eval $(parse_yaml "$node")
+    echo "=== $node ==="
+    wget -qO- "$endpoint/fdsnws/station/1/query?level=network&format=text" | awk -F\| '{print $1 " | " $2}' | sort | uniq -c | grep -v "1 "
+done
