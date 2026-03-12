45f626cddf3664797fb289b447a5204e78d462f5
Jonathan Schaeffer <jonathan.schaeffer@univ-grenoble-alpes.fr>
2026-03-12 12:48:10 +0100

    Adarray embargo roll test fix

--- src/test_adria_array_embargo.py
+++ src/test_adria_array_embargo.py
@@ -8,11 +8,18 @@
 # ]
 # ///
 
-import sys
 import requests
-from datetime import datetime
 from obspy.clients.fdsn import Client
-import xml.etree.ElementTree as ET
+from datetime import datetime
+
+
+def list_stations(dc: dict, net: str):
+    """
+    builds a list of stations from a datacenter and a network
+    """
+    stations = [p["sta"] for p in dc["params"] if p["net"] == net]
+    return stations
+
 
 # First fetch ADRIA ARRAY stations from the routing
 resp = requests.get(
@@ -30,19 +37,29 @@ for svc in inventory:
 
     networks = set([n["net"] for n in svc["params"]])
     for net in networks:
+        adarray_stations = list_stations(svc, net)
         client = Client(base_url=url.removesuffix("fdsnws/station/1/query"))
-        inventory = client.get_stations(net=net, level="channel")
+        inventory = client.get_stations(
+            net=net, level="channel", sta=",".join(adarray_stations)
+        )
 
         for net in inventory.networks:
             for sta in net.stations:
                 for cha in sta:
                     if cha.restricted_status == "closed":
                         if cha.start_date < embargo_start:
-                            restricted_channels.append(cha)
+                            restricted_channels.append(
+                                f"{net.code}_{sta.code}_{cha.location_code}_{cha.code} [{cha.restricted_status}] {cha.start_date} -> {cha.end_date}"
+                            )
 
     if len(restricted_channels) > 0:
         print("## ", url)
-        for cha in restricted_channels:
-            print(
-                f"{net.code}_{sta.code}_{cha.location_code}_{cha.code}: {cha.start_date} -> {cha.end_date}"
-            )
+        print("<details>")
+        print(
+            f"<summary>{len(restricted_channels)} channels need to be rolled out</summary>"
+        )
+        print()
+        for c in restricted_channels:
+            print(f"- {c}")
+        print("</details>")
+        print()
--- src/test_wfcatalog_issue_85.bash
+++ src/test_wfcatalog_issue_85.bash
@@ -5,7 +5,7 @@ source lib/nodes-tester.bash
 for node in $NODE_DEFINITIONS; do
     eval $(parse_yaml "$node")
     POST_DATA="$onlineCheck_net $onlineCheck_sta $onlineCheck_loc $onlineCheck_cha $onlineCheck_start $onlineCheck_end"
-    if http -F -q --check-status "$endpoint/eidaws/wfcatalog/1/query" <<<$POST_DATA >/dev/null 2>&1; then
+    if curl --connect-timeout 2 -fSs "https://$endpoint/eidaws/wfcatalog/1/query" --data "$POST_DATA" >/dev/null 2>&1; then
         echo "- [x] $node"
     else
         echo "- [ ] $node"
